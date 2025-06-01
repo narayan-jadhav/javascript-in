01781d42b9151c16a85ac38e83de131bd43d7174
narayan-jadhav <njadhav0821@gmail.com>
2025-06-01 16:28:49 +0530

    add

--- .devcontainer/01.basics/test.js
+++ .devcontainer/01.basics/test.js
@@ -1,1 +1,15 @@
 console.log("hii narayan");
+const accountId="1434"
+let accountEmail="njadhav0821@gmail.com"
+var accountPassword="1234"
+
+console.log(accountId);
+console.log(accountPassword);
+
+
+let name="        hello     "
+// console.log(name);
+let newname=name.touppercase();
+console.log("after touppercase"+newname);
+ newname=name.trim();
+ console.log("after trim"+newname);
